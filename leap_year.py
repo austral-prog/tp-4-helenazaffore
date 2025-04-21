ad88ad89c296ac60b50fea8adfc5d43bab47164a
helenazaffore <helenazaffore@gmail.com>
2025-04-21 04:10:30 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,4 @@
 def leap_year():
-    print("TO DO")
     anio = int(input("Ingrese un año: "))
     if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
         print(f"El año {anio} es bisiestro")
